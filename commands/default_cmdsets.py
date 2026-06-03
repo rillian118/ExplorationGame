78d09a0d119e5f6855a8561ef10ba8be26bcb0b0
rillian118 <patrick.mcnallen@gmail.com>
2026-06-03 18:38:04 -0500

    Committing in-game commands into Evennia

--- commands/default_cmdsets.py
+++ commands/default_cmdsets.py
@@ -14,7 +14,8 @@ own cmdsets by inheriting from them or directly from `evennia.CmdSet`.
 
 """
 
-from evennia import default_cmds
+from evennia import default_cmds # type: ignore
+from world.space.cmdsets import SpaceCmdSet
 
 
 class CharacterCmdSet(default_cmds.CharacterCmdSet):
@@ -34,6 +35,7 @@ class CharacterCmdSet(default_cmds.CharacterCmdSet):
         #
         # any commands you add below will overload the default ones.
         #
+        self.add(SpaceCmdSet())
 
 
 class AccountCmdSet(default_cmds.AccountCmdSet):
